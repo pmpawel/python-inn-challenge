9b09d30719251d140321dd254b8b62706866f4e9
Pawel Miszkurka <pawel@pmac.local>
2023-12-03 11:44:33 +0700

    fix logic of update_quality method to fix issues with quality value reset, Conjured items decrement and enforrcing quality limits; fix test

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -6,35 +6,46 @@ class GildedRose(object):
         self.items = items
 
     def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
 
+        for item in self.items:
+            if item.name == "Aged Brie":
+                if item.quality < 50:
+                    item.quality += 1
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in > 10:
+                    item.quality += 1
+                elif 5 < item.sell_in <= 10:
+                    item.quality += 2
+                elif 0 < item.sell_in <= 5:
+                    item.quality += 3
+
+
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                item.quality = 80
+                item.sell_in = 0
+                pass  # Sulfuras quality remains constant at 80
+            elif item.name == "Conjured Mana Cake":
+                if item.quality > 1:
+                    if item.sell_in <= 0:
+                        item.quality -= 4  # After sell-in date passes, quality degrades by 4
+                    else:
+                        item.quality -= 2  # Quality degrades by 2 for Conjured items
+            else:  # For regular items
+                if item.quality > 0:
+                    if item.sell_in <= 0:
+                        item.quality -= 2  # After sell-in date passes, quality degrades by 2
+                    else:
+                        item.quality -= 1
+
+            # Enforce quality limits
+            if item.quality < 0:
+                item.quality = 0
+            elif item.quality > 50 and item.name != "Sulfuras, Hand of Ragnaros":
+                item.quality = 50
+
+            # Update sell-in value for all items except Sulfuras
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                item.sell_in -= 1
 
 class Item:
     def __init__(self, name, sell_in, quality):
