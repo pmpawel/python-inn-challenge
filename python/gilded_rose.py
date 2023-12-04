7f76299b630aa43ffae4bdf3863439e5afd583ae
Pawel Miszkurka <pawelmiszkurka@gmail.com>
2023-12-04 17:29:39 +0700

    extra comments

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -65,14 +65,19 @@ class Backstage(Item):
         super().__init__(name, sell_in, quality)
 
     def update_quality(self):
+        # Increase item requirement by 1 as normal when sellin is more than 10
         if self.sell_in > 10:
             self.quality += 1
+        # possibly better apporach would be to use range
+        # value increases by 2 when sell in 6-10
         elif 5 < self.sell_in <= 10:
             self.quality += 2
+        # value increases by 3 when sell in 1-5    
         elif 0 < self.sell_in <= 5:
             self.quality += 3
         else:
-            self.quality = 0
+            # concert
+            self.quality = 0 
 
         if self.quality > 50:
             self.quality = 50
@@ -84,9 +89,11 @@ class ConjuredItem(Item):
     
     def update_quality(self):
         if self.quality > 1:
+            # quality degrades twice as fast after sell_in data pass below 0
             if self.sell_in <= 0:
                 self.quality -= 4  # After sell-in date passes, quality degrades by 4
             else:
+                # quality decreases by 2 in normal when product within sell in > 0
                 self.quality -= 2  # Quality degrades by 2 for Conjured items
         self.sell_in -= 1    
 
